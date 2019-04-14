3a131d796ec276dc5419a590d21b79ad6be88983
Abramov Ivan <abramov9647@yandex.ru>
2019-04-14 19:43:29 +0300

    Junior

---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"testing"
+)
+
+var testOk = `1
+2
+3
+4
+5`
+var testOkResult = `1
+2
+3
+4
+5
+`
+
+func TestOk(t *testing.T) {
+	in := bufio.NewReader(strings.NewReader(testOk))
+	out := new(bytes.Buffer)
+	err := uniq(in, out)
+	if err != nil {
+		t.Errorf("test for OK Failed")
+	}
+	result := out.String()
+	if result != testOkResult {
+		t.Errorf("test for OK Failed - result not math %v %v", result, testOkResult)
+	}
+}
+
+var testFail = `1
+2
+1`
+
+func testForError(t *testing.T) {
+	in := bufio.NewReader(strings.NewReader(testFail))
+	out := new(bytes.Buffer)
+	err := uniq(in, out)
+	if err == nil {
+		t.Errorf("test for OK Failed - error: %v", err)
+	}
+}
